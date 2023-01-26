eef5a45899b0323af3c8c225e231b8364989308a
Alexis Drai <alexis.drai@etu.uca.fr>
2023-01-26 11:09:00 +0100

    :construction: WIP on protocols for rules

--- Connect4/connect4_lib/Sources/connect4_lib/Board.swift
+++ Connect4/connect4_lib/Sources/connect4_lib/Board.swift
@@ -1,5 +1,6 @@
 import Foundation
 
+//TODO implement Equatable?
 public struct Board : CustomStringConvertible {
     
     public let nbRows: Int
---
+++ Connect4/connect4_lib/Sources/connect4_lib/IRules.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+protocol IRules {
+    var minNbRows: Int { get }
+    var maxNbRows: Int { get }
+    var minNbCols: Int { get }
+    var maxNbCols: Int { get }
+    var nbChipsToAlign: Int { get }
+    func isGameOver(byPlayer playerId: Int, ontGrid grid: [[Int?]]) -> (isOver: Bool, hasWinner: Bool, victoryTiles: [(Int?, Int?)])
+}
