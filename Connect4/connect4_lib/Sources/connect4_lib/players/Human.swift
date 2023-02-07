9ae6386877b3b4564adb5b0c24d308cbe651c424
Alexis Drai <alexis.drai@etu.uca.fr>
2023-02-07 14:52:34 +0100

    :art: prettify Human

--- Connect4/connect4_lib/Sources/connect4_lib/players/Human.swift
+++ Connect4/connect4_lib/Sources/connect4_lib/players/Human.swift
@@ -3,12 +3,16 @@ class Human: Player {
     
     private let scanner: () -> Int?
     
-    init(withId id: Int, withName name: String, usingScanner scanner: @escaping () -> Int?){
+    init(withId id: Int,
+         withName name: String,
+         usingScanner scanner: @escaping () -> Int?) {
         self.scanner = scanner
         super.init(withId: id, withName: name)
     }
 
-    override func chooseColumn(inBoard board: Board, withRules rules: IRules) -> Int? {
+    override func chooseColumn(inBoard board: Board,
+                               withRules rules: IRules)
+    -> Int? {
         return scanner()
     }
 }
